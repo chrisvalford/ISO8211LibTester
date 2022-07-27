dae55b6f51531e2978fc2c020346fd6c340e60f5
Christopher Alford <chrisvalford@me.com>
2022-07-27 10:20:46 +0200

    Initial Commit

---
+++ ISO8211LibTester/ISO8211LibTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISO8211LibTesterApp.swift
+//  ISO8211LibTester
+//
+//  Created by Christopher Alford on 27/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ISO8211LibTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ISO8211LibTesterUITests/ISO8211LibTesterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ISO8211LibTesterUITestsLaunchTests.swift
+//  ISO8211LibTesterUITests
+//
+//  Created by Christopher Alford on 27/7/22.
+//
+
+import XCTest
+
+final class ISO8211LibTesterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
